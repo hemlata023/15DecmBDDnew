5242efc1554c439d4a188d4439aff5809ce87ccc
Administrator <Administrator@172.31.5.23>
2023-12-19 07:52:24 +0000

    new commit

--- src/test/java/stepDefs/CRMStepDef.java
+++ src/test/java/stepDefs/CRMStepDef.java
@@ -7,12 +7,24 @@ import io.cucumber.java.After;
 public class CRMStepDef {
 	
 	@Given("User is on LoginPage")
-	public void user_is_on_loginpage() {
-		System.out.println("User is on LoginPage");
+	public void user_is_on_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
+	
+	@Given("User is on loginpage")
+	public void user_is_on_loginpage() {
+		System.out.println("User is on loginpage");
 	    
-	}	
-	    
+	}
+
+	@When("User  create a new contact")
+	public void user_create_a_new_contact() {
+		System.out.println("User  create a new contact");
+	    	}
+
+
 	
 	@When("User  view a contact  details")
 	public void user_view_a_contact_details() {
